0b218f3aa1896c3708996b8dcfb3286609238d62
Vitalya3553 <92946668+Vitalya3553@users.noreply.github.com>
2022-12-27 13:48:03 +0100

    Update FieldHandler.kt

--- FieldHandler.kt
+++ FieldHandler.kt
@@ -1,16 +1,30 @@
-    data class FieldProperties(val editText : TextInputEditText,
-                               val inputLayout: TextInputLayout,
-                               val errorText: String,
-                               val rule : ()->Boolean
+
+
+class FieldHandler(private vararg val invalidFieldHandler: FieldProperties, private val timer: Long?) {
+
+    private lateinit var condition: () -> Boolean
+    private var isSleeping = MutableStateFlow(false)
+    private var sleepingJob : Job? = null
+
+    data class FieldProperties(
+        val editText: TextInputEditText,
+        val inputLayout: TextInputLayout,
+        val errorText: String,
+        val rule: (() -> Boolean)? = null,
+        val execute: (() -> Unit)? = null,
     ){
 
         override fun equals(other: Any?): Boolean {
-
             return if (other is FieldProperties) {
                 inputLayout == other.inputLayout || editText == other.editText
             } else {
                 false
             }
+        }
+
+        override fun hashCode(): Int {
+
+            return inputLayout.hashCode() + editText.hashCode()
         }
     }
     class UsingTheSameObjectsError : Error("You are using the same TextInputLayout or TextInputEditText objects")
@@ -23,6 +37,25 @@
         setErrorDrawables(0)
         setListeners()
     }
+
+    private  fun setTimer(code: () -> Unit) {
+        if(timer == null){
+            return
+        }
+        if (sleepingJob!= null && sleepingJob!!.isActive){
+            sleepingJob!!.cancel()
+        }
+        sleepingJob = CoroutineScope(Dispatchers.Main).launch {
+            if(!isSleeping.value) isSleeping.value = true
+
+            delay(timer)
+
+            isSleeping.value = false
+
+            code()
+        }
+    }
+
     private fun checkDuplicate(){
         if (invalidFieldHandler.count() != invalidFieldHandler.distinct().count()){
             throw UsingTheSameObjectsError()
@@ -38,23 +71,35 @@
             it.editText.addTextChangedListener(object : TextWatcher {
                 override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
                 override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                    if( it.rule()) {
-                        it.inputLayout.isErrorEnabled = false
-                    }
+                   setTimer(){
+                       Log.e("Field Handler", "TExt changed")
+                       condition  = (it.rule ?:  {true})
+                       if(condition()) {
+                           it.inputLayout.isErrorEnabled = false
+                       }
+
+                       it.execute?.let { it1 -> it1() }
+                   }
+
                 }
                 override fun afterTextChanged(s: Editable?) {}
 
             })
-
-            it.editText.setOnFocusChangeListener { v, hasFocus ->
-                if(!hasFocus){
-                    if(!it.rule()){
-                        it.inputLayout.isErrorEnabled = true
-                        it.inputLayout.error = it.errorText
+            if(timer == null){
+                it.editText.setOnFocusChangeListener { v, hasFocus ->
+                    if(!hasFocus){
+                        condition = it.rule ?:  {true}
+                        if(!condition()){
+                            it.inputLayout.isErrorEnabled = true
+                            it.inputLayout.error = it.errorText
+                        }
                     }
                 }
             }
+
         }
+
+
     }
     private fun setErrorDrawables(drawable: Int){
         invalidFieldHandler.map {
@@ -70,8 +115,8 @@
 
             invalidFieldHandler.map {
 
-
-                if(!it.rule()){
+                condition = (it.rule ?:  {true})
+                if(!condition()){
 
                     isCorrect = false
 
@@ -89,10 +134,12 @@
 
         }else{
             invalidFieldHandler.map {
-                if(!it.rule()) return false
+                condition = (it.rule ?:  {true}) as () -> Boolean
+                if(!condition()) return false
 
             }
             return true
         }
 
     }
+}
